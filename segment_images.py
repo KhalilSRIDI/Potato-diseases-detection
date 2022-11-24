09068161bbe8408f51876b8a59e6f6a6cc3387df
KhalilSRIDI <khalil.sridi@esprit.tn>
2022-11-24 20:37:47 +0100

    arguments
    added arguments to run script from cmd

--- segment_images.py
+++ segment_images.py
@@ -1,16 +1,23 @@
 import cv2
 import numpy as np
 import os
+import sys
+import re
 
 
-"""sumary_line
-
-Keyword arguments:
-argument -- description
-Return: return_description
-"""
-
-
+n = len(sys.argv)
+for i in range(1, n):
+    if n == 1 or sys.argv[i] == '-h':
+        print('Arg1 : segment one image or whole dataset (-f or -d)\n')
+        print('Arg2 : segmentation type: normal or HSV (-n or -hsv)\n')
+        print('Arg3 : path (must be between double quotes "" )\n')
+        exit()
+    else:
+        nature=sys.argv[1]
+        type=sys.argv[2]
+        path=sys.argv[3]
+        continue
+   
 def segment_image(file):
   """
   segment a given image,remove it's backgreound and save the output result
@@ -33,8 +40,9 @@ def segment_image(file):
   disease_result = cv2.bitwise_and(img, img, mask=disease_mask)
   final_mask = mask + disease_mask
   final_result = cv2.bitwise_and(img, img, mask=final_mask)
-  output_path = '/content/drive/MyDrive/datasetCBL/Segmented/' + \
-      os.path.basename(os.path.dirname(file))+'/'+os.path.basename(file)
+  new_path = str(path.rsplit("\\",3)[0])
+  dir_path=os.path.join(new_path,"Segmented")
+  output_path = new_path+os.path.basename(os.path.dirname(file))+"\\"+os.path.basename(file)
   cv2.imwrite(output_path, final_result)
 
 
@@ -48,7 +56,9 @@ def segment_image_HSV(file):
     """
     img = cv2.imread(file)
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    output_path='/content/drive/MyDrive/datasetCBL/Segmented_HSV/'+os.path.basename(os.path.dirname(file))+'/'+os.path.basename(file)
+    new_path = str(path.rsplit("\\",3)[0])
+    dir_path=os.path.join(new_path,"Segmented_HSV")
+    output_path = new_path+os.path.basename(os.path.dirname(file))+"\\"+os.path.basename(file)
     cv2.imwrite(output_path,hsv_img)
 
 
@@ -65,12 +75,26 @@ def convert_dataset(rootdir,type):
     count=0
     for subdir, dirs, files in os.walk(rootdir):
         for file in files:
-            path=str(subdir)+'/'+str(file)
+            fpath=str(subdir)+'\\'+str(file)
             if type == 'Color':
-                segment_image(path)
+                segment_image(fpath)
                 count+=1
             else:
-                segment_image_HSV(path)
+                segment_image_HSV(fpath)
                 count+=1
 
+
     print('segmented '+str(count)+' images correctly')  
+    
+if nature=='-f':
+    print('done')
+    if type =='-n':
+        segment_image(path)
+    else:
+        segment_image_HSV(path)
+else:
+    dirpath=path+'\\'
+    if type =='-n':   
+        convert_dataset(dirpath,'Color')
+    else:
+        convert_dataset(dirpath,'Segmented')
